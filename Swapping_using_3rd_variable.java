c50ada131a04d7ee47521c8445c77cbb92f0bbdf
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-03-13 23:49:52 +0530

    Create Swapping_using_3rd_variable.java

---
+++ Swapping_using_3rd_variable.java
@@ -0,0 +1,13 @@
+public class Swapping_using_3rd_variable
+{
+    public static void main(String[]args)
+    {
+        int a = 10;
+        int b = 20;
+        int c;
+        c=a;
+        a=b;
+        b=c;
+        System.out.println(a+" "+b);
+    }
+}
