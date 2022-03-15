2de55610c7bae3c764180c6fd0ca8a45ed9af79d
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-03-15 23:37:20 +0530

    prime or not
    to check weather a user input number is prime or not

---
+++ prime_or_not.java
@@ -0,0 +1,27 @@
+package placement.questions;
+import java.util.Scanner;
+public class Prime_or_not
+{
+    public static void main(String[]args)
+    {
+        Scanner scr= new Scanner(System.in);
+        System.out.println("for what number you want to check weather the given number is prime or not");
+        int n = scr.nextInt();
+        int temp =0;
+     for (int i = 2; i<n-1; i++)
+     {
+         if (n % 2 ==0)
+         {
+             temp = temp +1;
+         }
+     }
+         if(temp>1)
+         {
+             System.out.println("this is not prime");
+         }
+         else
+         {
+             System.out.println("this is prime");
+         }
+    }
+}
