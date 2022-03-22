abe333d371be28dc481788eb4fa00d4dc946649f
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-03-22 19:52:23 +0530

    Create Character_pattern_01.java
    Printed the value of row

---
+++ pattern/Character_pattern_01.java
@@ -0,0 +1,21 @@
+public class Character_pattern01
+{
+    public static void main(String[]args)
+    {
+        char i,j;
+        for (i='A'; i<= 'E'; i++)
+        {
+            for(j='A'; j<= 'E'; j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
+//output
+//AAAAA
+//BBBBB
+//CCCCC
+//DDDDD
+//EEEEE
