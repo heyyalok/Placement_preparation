10894684b0bfb3651e6f9efedab5d5bafaf1746f
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-03-22 14:43:08 +0530

    mirror image of the right angled triangle
    //    *
    //   **
    //  ***
    // ****
    //*****

---
+++ pattern/mirror_image_of_the_right_angled_triangle.java
@@ -0,0 +1,25 @@
+public class Interview_question_04
+{
+    public static void main(String[]args)
+    {
+        int n = 5;
+        for (int i = 1; i<=n; i++)
+        {
+            for(int j = n-i; j>=1; j--)
+            {
+                System.out.print(" ");
+            }
+            for (int k = 1; k<= i;k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+//output
+//    *
+//   **
+//  ***
+// ****
+//*****
