d0621705eb810cc37125245f55cfd90fb67040c1
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-03-22 19:56:44 +0530

    Create Character_pattern02.java
    printed columns only 
    //output
    //ABCDE
    //ABCDE
    //ABCDE
    //ABCDE
    //ABCDE

---
+++ pattern/Character_pattern02.java
@@ -0,0 +1,21 @@
+public class Character_pattern02
+{
+    public static void main(String[]args)
+    {
+        char i,j;
+        for (i='A'; i<= 'E';i++)
+        {
+            for (j='A'; j<='E'; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+//output
+//ABCDE
+//ABCDE
+//ABCDE
+//ABCDE
+//ABCDE
