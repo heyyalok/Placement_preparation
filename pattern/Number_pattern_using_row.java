ecc9332b7cfdf473120f0fdd52da271c6307678b
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-03-22 18:31:31 +0530

    Create Number_pattern_using_row.java
    Number pattern printing 
    11111
    22222
    33333
    44444
    55555

---
+++ pattern/Number_pattern_using_row.java
@@ -0,0 +1,20 @@
+public class Number_pattern02
+{
+    public static void main(String[]args)
+    {
+        for (int i=1;i<=5;i++)
+        {
+            for (int j=1;j<=5;j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
+//output
+//11111
+//22222
+//33333
+//44444
+//55555
