d6c3a6a6133c2e3908d40c4b18bf5b5c753112e2
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-03-23 18:45:53 +0530

    Create Character_pattern_03.java
    //eeeee
    //dddd
    //ccc
    //bb
    //a

---
+++ pattern/Character_pattern_03.java
@@ -0,0 +1,20 @@
+public class Trial
+{
+    public static void main(String[]args)
+    {
+        char i,j;
+        for (i='e';i>='a';i--)
+        {
+            for (j='a';j<=i;j++)
+            {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+//eeeee
+//dddd
+//ccc
+//bb
+//a
