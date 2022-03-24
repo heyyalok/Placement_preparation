605a7bf487ec138d604f1f16414f57eeb6d54654
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-03-24 21:36:48 +0530

    Create Plus_symbool_using_if_else.java
    //output
    //  *  
    //  *  
    //*****
    //  *
    //  *

---
+++ Plus_symbool_using_if_else.java
@@ -0,0 +1,28 @@
+public class Youtube_classes 
+{
+    public static void main(String[] args)
+    {
+        for (int i = 1; i<= 5; i++)
+        {
+            for(int j =1; j<= 5; j++)
+            {
+                if(i==3 || j==3)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+}
+//output
+//  *  
+//  *  
+//*****
+//  *
+//  *
