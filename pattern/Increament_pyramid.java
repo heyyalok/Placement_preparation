3cd2fae8fada0be1cbce704abf34c434b403afca
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-03-25 11:25:58 +0530

    Create Increament_pyramid.java
    //output
    //   *
    //  ***
    // *****
    //*******

---
+++ pattern/Increament_pyramid.java
@@ -0,0 +1,25 @@
+public class loopcontrol 
+{
+public static void main(String[]args)
+{
+    int z  = 1;
+    for (int i=1; i<= 4; i++)
+    {
+        for (int j = 3; j>= i; j--)
+        {
+            System.out.print(" ");
+        }
+       for (int k = 1; k<= z; k++)
+       {
+           System.out.print("*");
+       } 
+       z=z+2;
+       System.out.println();
+    }
+}    
+}
+//output
+//   *
+//  ***
+// *****
+//*******
