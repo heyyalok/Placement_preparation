f2e6457ff73a18f79c1b4795dd6e5662654340f1
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-03-26 13:01:02 +0530

    Create Number_pattern_j_horizontal_pyramid.java
    //1
    //12
    //123
    //1234
    //123
    //12
    //1

---
+++ pattern/Number_pattern_j_horizontal_pyramid.java
@@ -0,0 +1,30 @@
+public class Loop_pattern 
+{
+    public static void main(String[]args)
+    {
+        for (int i = 1; i<= 4; i++)
+        {
+            for (int j = 1; j<= i; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        for (int i =3; i>= 1; i--)
+        {
+            for (int j = 1; j<= i; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+//output
+//1
+//12
+//123
+//1234
+//123
+//12
+//1
