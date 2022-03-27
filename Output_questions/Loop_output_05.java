216edeea3cec2acd5375b998214b971ac3653a48
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-03-27 22:53:51 +0530

    Create Loop_output_05.java
    //output
    // 4

---
+++ Output_questions/Loop_output_05.java
@@ -0,0 +1,14 @@
+public class Loop_output_05
+{
+    public static void main(String[]args)
+    {   int b =10;
+        for (int a= 1;a<=4;a++)
+    {
+        b=b+a;
+    }
+        int c=b/5;
+        System.out.print(c);
+    }   
+}
+//output
+// 4
